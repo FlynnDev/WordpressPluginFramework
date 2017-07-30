c5029228b70d8672aa4342363259308d26488435
Mike Flynn <spikeon@gmail.com>
2017-07-29 21:28:43 -0700

    -m

--- dist/load.php
+++ dist/load.php
@@ -1,36 +1,34 @@
 <?php
 /**
  * Package:  WordPress Plugin Framework
- * Version:  1.1.111
+ * Version:  1.1.112
  * Date:     29-07-2017
  * Copyright 2017 Mike Flynn - mflynn@flynndev.us
  */ 
  
 
 
+namespace PluginFramework\V_1_1;
+
 // Attempt to tell server to allow url fopen
 ini_set("allow_url_fopen", 1);
-
-if(!function_exists('plugin_framework_check_version')) {
-	/**
-	 * Check PHP deps
-	 *
-	 * @return bool
-	 */
-	function plugin_framework_check_version() {
-		return ! version_compare( PHP_VERSION, '5.4.0', '<' );
-	}
-}
 
 if(! function_exists("plugin_core_error_admin_notice")) {
 
 	function plugin_core_error_admin_notice() {
 		$class   = 'notice notice-error';
-		$message = __( 'The plugin ', 'plugin-core' );
+		$message = __( 'The plugins ' . implode(', ', $GLOBALS['plugin_framework_plugins']) . ' require PHP 5.4.0', 'plugin-core' );
 
 		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
 	}
 
+}
+
+if(! function_exists("plugin_framework_register")) {
+	function plugin_framework_register($name, $file){
+		if(empty($GLOBALS['plugin_framework_plugins'])) $GLOBALS['plugin_framework_plugins'] = [];
+		$GLOBALS['plugin_framework_plugins'][$name] = $file;
+	}
 }
 
 if( plugin_framework_check_version() == false ) {
--- src/plugin_framework_check_version.function.php
+++ src/plugin_framework_check_version.function.php
@@ -1,3 +1,12 @@
 <?php
-
-
+	namespace PluginFramework;
+	if(!function_exists('plugin_framework_check_version')) {
+		/**
+		 * Check PHP deps
+		 *
+		 * @return bool
+		 */
+		function plugin_framework_check_version() {
+			return ! version_compare( PHP_VERSION, '5.4.0', '<' );
+		}
+	}
