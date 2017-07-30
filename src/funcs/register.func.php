afd06d5f4ed93731ddbdf991073b5f8340218a24
Mike Flynn <spikeon@gmail.com>
2017-07-29 22:36:45 -0700

    -m

--- src/funcs/register.func.php
+++ src/funcs/register.func.php
@@ -3,7 +3,7 @@ namespace PluginFramework;
 
 if(! function_exists(__NAMESPACE__ . "\register")) {
 	function register($name, $file){
-		if(empty($GLOBALS['plugin_framework_plugins'])) $GLOBALS['plugin_framework_plugins'] = [];
+		if(empty($GLOBALS['plugin_framework_plugins'])) $GLOBALS['plugin_framework_plugins'] = array();
 		$GLOBALS['plugin_framework_plugins'][$name] = $file;
 	}
 }
--- src/load.php
+++ src/load.php
@@ -3,31 +3,37 @@
 
 namespace PluginFramework;
 
-// Attempt to tell server to allow url fopen
-ini_set("allow_url_fopen", 1);
-
 if(empty($GLOBALS['plugin_framework_phpver'])) $GLOBALS['plugin_framework_phpver'] = '5.4.0';
 
 require_once("funcs/check_version.func.php");
-require_once("funcs/register.func.php");
-require_once("funcs/phpver.func.php");
 require_once("funcs/admin_notice.func.php");
+require_once("funcs/phpver.func.php");
+require_once("funcs/register.func.php");
 
 if( ! check_version() ) {
-	add_action( 'admin_notices', __NAMESPACE__ . '\admin_notice' );
+	add_action( 'admin_notices', __NAMESPACE__ . '\admin_notice_ver' );
 }
 else {
-	if ( ! class_exists( __NAMESPACE__ . "\Core" ) ) {
-		require_once( 'mustache.php' );
 
-		$folder = dirname( __FILE__ ) . '/' . 'traits';
-		foreach ( scandir( $folder ) as $filename ) {
-			$path = $folder . '/' . $filename;
-			if ( is_file( $path ) ) {
-				require_once( $path );
+	require_once("funcs/php_settings.func.php");
+
+	if ( ! php_settings() ){
+		add_action( 'admin_notices', __NAMESPACE__ . '\admin_notice_settings' );
+	}
+	else {
+
+		if ( ! class_exists( __NAMESPACE__ . "\Core" ) ) {
+			require_once( 'mustache.php' );
+
+			$folder = dirname( __FILE__ ) . '/' . 'traits';
+			foreach ( scandir( $folder ) as $filename ) {
+				$path = $folder . '/' . $filename;
+				if ( is_file( $path ) ) {
+					require_once( $path );
+				}
 			}
-		}
 
-		require_once( 'core.class.php' );
+			require_once( 'core.class.php' );
+		}
 	}
 }
