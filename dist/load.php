469de1073d967333cf9a43ea7892da6a2380cba0
Mike Flynn <spikeon@gmail.com>
2017-07-29 21:01:58 -0700

    -m

--- dist/load.php
+++ dist/load.php
@@ -1,27 +1,50 @@
 <?php
 /**
  * Package:  WordPress Plugin Framework
- * Version:  1.1.108
- * Date:     27-07-2017
+ * Version:  1.1.109
+ * Date:     29-07-2017
  * Copyright 2017 Mike Flynn - mflynn@flynndev.us
  */ 
  
 
 
-if(!class_exists("PluginFramework\V_1_1\Core")) {
-	require_once( 'mustache.php' );
+if(!function_exists('plugin_framework_check_version')) {
+	/**
+	 * Check PHP deps
+	 *
+	 * @return bool
+	 */
+	function plugin_framework_check_version() {
+		return version_compare( PHP_VERSION, '5.4.0', '<' );
+	}
+}
 
-	$folder = dirname(__FILE__) . '/' . 'traits';
-	foreach (scandir( $folder) as $filename) {
-		$path = $folder . '/' . $filename;
-		if (is_file($path)) require_once ($path);
+if(! function_exists("plugin_core_error_admin_notice")) {
+
+	function plugin_core_error_admin_notice() {
+		$class   = 'notice notice-error';
+		$message = __( 'The plugin ', 'plugin-core' );
+
+		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
 	}
-/*
-	$folder = dirname(__FILE__) . '/' . 'classes';
-	foreach (scandir($folder) as $filename) {
-		$path = $folder . '/' . $filename;
-		if (is_file($path)) require_once ($path);
+
+}
+
+if( plugin_framework_check_version() ) {
+	add_action( 'admin_notices', 'plugin_core_error_admin_notice' );
+}
+else {
+	if ( ! class_exists( "PluginFramework\V_1_1\Core" ) ) {
+		require_once( 'mustache.php' );
+
+		$folder = dirname( __FILE__ ) . '/' . 'traits';
+		foreach ( scandir( $folder ) as $filename ) {
+			$path = $folder . '/' . $filename;
+			if ( is_file( $path ) ) {
+				require_once( $path );
+			}
+		}
+
+		require_once( 'core.class.php' );
 	}
-*/
-	require_once( 'core.class.php' );
 }
--- src/load.php
+++ src/load.php
@@ -1,20 +1,43 @@
 <?php
 //banner
 
-if(!class_exists("PluginFramework\Core")) {
-	require_once( 'mustache.php' );
+if(!function_exists('plugin_framework_check_version')) {
+	/**
+	 * Check PHP deps
+	 *
+	 * @return bool
+	 */
+	function plugin_framework_check_version() {
+		return version_compare( PHP_VERSION, '5.4.0', '<' );
+	}
+}
 
-	$folder = dirname(__FILE__) . '/' . 'traits';
-	foreach (scandir( $folder) as $filename) {
-		$path = $folder . '/' . $filename;
-		if (is_file($path)) require_once ($path);
+if(! function_exists("plugin_core_error_admin_notice")) {
+
+	function plugin_core_error_admin_notice() {
+		$class   = 'notice notice-error';
+		$message = __( 'The plugin ', 'plugin-core' );
+
+		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
 	}
-/*
-	$folder = dirname(__FILE__) . '/' . 'classes';
-	foreach (scandir($folder) as $filename) {
-		$path = $folder . '/' . $filename;
-		if (is_file($path)) require_once ($path);
+
+}
+
+if( plugin_framework_check_version() ) {
+	add_action( 'admin_notices', 'plugin_core_error_admin_notice' );
+}
+else {
+	if ( ! class_exists( "PluginFramework\Core" ) ) {
+		require_once( 'mustache.php' );
+
+		$folder = dirname( __FILE__ ) . '/' . 'traits';
+		foreach ( scandir( $folder ) as $filename ) {
+			$path = $folder . '/' . $filename;
+			if ( is_file( $path ) ) {
+				require_once( $path );
+			}
+		}
+
+		require_once( 'core.class.php' );
 	}
-*/
-	require_once( 'core.class.php' );
 }
