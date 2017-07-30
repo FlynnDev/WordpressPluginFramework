dd68cd2fb6cec49974a16d6a5f613b2bf4f07318
Mike Flynn <spikeon@gmail.com>
2017-07-29 21:52:45 -0700

    -m

--- src/load.php
+++ src/load.php
@@ -9,9 +9,9 @@ ini_set("allow_url_fopen", 1);
 require_once("funcs/check_version.func.php");
 require_once("funcs/register.func.php");
 
-if(! function_exists("plugin_core_error_admin_notice")) {
+if(! function_exists(__NAMESPACE__ . "\admin_notice")) {
 
-	function plugin_core_error_admin_notice() {
+	function admin_notice() {
 		$class   = 'notice notice-error';
 		$message = __( 'The plugins ' . implode(', ', $GLOBALS['plugin_framework_plugins']) . ' require PHP 5.4.0', 'plugin-core' );
 
@@ -20,11 +20,11 @@ if(! function_exists("plugin_core_error_admin_notice")) {
 
 }
 
-if( plugin_framework_check_version() == false ) {
-	add_action( 'admin_notices', 'plugin_core_error_admin_notice' );
+if( check_version() == false ) {
+	add_action( 'admin_notices', __NAMESPACE__ . '\admin_notice' );
 }
 else {
-	if ( ! class_exists( "PluginFramework\Core" ) ) {
+	if ( ! class_exists( __NAMESPACE__ . "\Core" ) ) {
 		require_once( 'mustache.php' );
 
 		$folder = dirname( __FILE__ ) . '/' . 'traits';
