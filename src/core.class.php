7fcdedb0ba9c1f9e9a622e8c2655ea918e84cc03
Mike Flynn <spikeon@gmail.com>
2017-08-01 14:51:11 -0700

    -m

--- src/core.class.php
+++ src/core.class.php
@@ -6,7 +6,7 @@ use PluginFramework\ShortCodes\Core as Shortcodes;
 
 abstract class Core {
 
-	use PluginInfo, Helpers, Hooks, Errors, View, Data, Shortcodes, Security, Styles, Scripts, Pages, Filters, MetaBox;
+	use PluginInfo, Helpers, Hooks, Errors, View, Data, Shortcodes, Security, Styles, Scripts, Pages, Filters; //, MetaBox;
 
 	public $_t;
 	/**
@@ -20,7 +20,7 @@ abstract class Core {
 		$this->_t = $t;
 		$this->setPrefix($name);
 		$this->setShortcodePrefix($this->getPrefix());
-		$this->setMetaBoxPrefix($this->getPrefix());
+		// $this->setMetaBoxPrefix($this->getPrefix());
 		$this->setVersion($ver);
 		$this->setRoot($file);
 
@@ -30,7 +30,7 @@ abstract class Core {
 		$this->init_hooks();
 		$this->init_shortcodes();
 		$this->init_filters();
-		$this->init_metaboxes();
+		// $this->init_metaboxes();
 	}
 
 }
